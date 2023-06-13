bedaf6a57abdda1640d044a831d8391e481a5ebe
Dimitri Lilov <dev.dmtrllv@gmail.com>
2023-06-13 11:09:31 +0200

    updated .gitignore and added mongoose

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+dotenv.config(); // read .env files
+
+const {
+	DB_USER,
+	DB_PASSWORD
+} = process.env;
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@express-todo-app.2yhjoxl.mongodb.net/?retryWrites=true&w=majority`).then(() => console.log('Connected!'));
 
 const HOST = "127.0.0.1";
 const PORT = 3001;
+
 
 const app = express();
 
